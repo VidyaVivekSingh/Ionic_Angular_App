f851177c022049af603f6585400ed7af7367e54c
Vidyanand Sagar <sagar.vivek159@gmail.com>
2019-12-19 15:57:01 +0530

    Use of ngONDestroy to prevent memory leaks due to usage of Subject

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { RecipesService } from './recipes.service';
 import { Router, Event, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss']
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
 
   constructor(private router: Router, private RecipeService: RecipesService) { }
+  private recipeSubs: Subscription;
   recipes: Recipe[];
   ngOnInit() {
     // this.router.events.subscribe((event: Event) => {
@@ -19,8 +21,12 @@ export class RecipesPage implements OnInit {
     //   }
     // });
     this.recipes = this.RecipeService.getAllRecipe();
-    this.RecipeService.recipeChanged.subscribe(recipe => {
+    this.recipeSubs = this.RecipeService.recipeChanged.subscribe(recipe => {
       this.recipes = recipe;
     });
+  }
+
+  ngOnDestroy() {
+    this.recipeSubs.unsubscribe();
   }
 }
